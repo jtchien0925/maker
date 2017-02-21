f1ea875547ee6a747ce416ac7f133de5332aec02
jtchien0925 <jchien2@emory.edu>
2017-02-21 15:04:09 -0500

    add parameter

---
+++ maker_run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /escratch4/spakala/spakala_Jan_26/coatneyi_annotation2 
+
+export LD_LIBRARY_PATH=/usr/local/mpich2/1.4.1p1/gcc_4.4.4/lib:${LD_LIBRARY_PATH}
+/usr/local/mpich2/1.4.1p1/gcc_4.4.4/bin/mpirun -np $NSLOTS /usr/local/maker/latest/bin/maker maker_opts.ctl maker_bopts.ctl maker_exe.ctl
